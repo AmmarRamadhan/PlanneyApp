e45acf7ddac3e6a0f9b69e2be394b45a73a43832
ammarRamadhan <ammar.ramadhan@my.sampoernauniversity.ac.id>
2022-11-01 22:33:26 +0700

    Commit created a splashscreen

--- app/src/main/java/com/example/planneyapp/MainActivity.java
+++ app/src/main/java/com/example/planneyapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.planneyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int loading_time=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(MainActivity.this, ActivityLogin.class);
+                startActivity(home);
+                finish();
+            }
+        },loading_time);
+
     }
 }
